6836b652bc77e35ad5212d0bbc8308e672725809
TriCodeIT <bayu165kusuma@gmail.com>
2021-08-26 17:13:08 +0700

    React CoreUI Admin Template

---
+++ src/containers/_nav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import CIcon from "@coreui/icons-react";
+
+const _nav = [
+  {
+    _tag: "CSidebarNavItem",
+    name: "Dashboard",
+    to: "/dashboard",
+    icon: <CIcon name="cil-speedometer" customClasses="c-sidebar-nav-icon" />,
+    badge: {
+      color: "info",
+      text: "NEW",
+    },
+  },
+  {
+    _tag: "CSidebarNavDropdown",
+    name: "Products",
+    route: "/products",
+    icon: "cil-puzzle",
+    _children: [
+      {
+        _tag: "CSidebarNavItem",
+        name: "All Products",
+        to: "/products/all-products",
+      },
+      {
+        _tag: "CSidebarNavItem",
+        name: "Add New Products",
+        to: "/products/add-new-products",
+      },
+    ],
+  },
+];
+
+export default _nav;
---
+++ src/views/products/AllProducts/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllProducts = (props) => {
+  return <div>Hello I am All Products</div>;
+};
+
+export default AllProducts;
